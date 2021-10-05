f0b5b674c78526ed484d00fc9230f3ceb8967af5
jvallexm <jvallexm@gmail.com>
2021-10-05 11:20:51 -0400

    Update iam

--- iam/invite_users.tf
+++ iam/invite_users.tf
@@ -2,15 +2,8 @@
 # Invite Users
 ##############################################################################
 
-locals {
-    access_groups_with_invites = {
-        for group in local.access_groups_object:
-        group.name => group if group.invite_users != null 
-    }
-}
-
 resource ibm_iam_user_invite invite {
-    for_each = local.access_groups_with_invites
+    for_each = local.access_groups_object
     users    = each.value.invite_users
 }
 
@@ -22,7 +15,7 @@ resource ibm_iam_user_invite invite {
 ##############################################################################
 
 resource ibm_iam_access_group_members group_members {
-    for_each        = local.access_groups_with_invites
+    for_each        = local.access_groups_object
     access_group_id = ibm_iam_access_group.groups[each.key].id
     ibm_ids         = each.value.invite_users
     depends_on = [
